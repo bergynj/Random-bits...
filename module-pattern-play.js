fe06efa2970879ba9befba94febec649f246af6d
Nathanael Utama <nathan.utama@gmail.com>
2011-11-01 11:27:09 +1100

    first commit - play with module pattern

---
+++ module-pattern-play.js
@@ -0,0 +1,52 @@
+// global namespace
+var app = app || {};
+$j = jQuery.noConflict();
+//$j(document).ready(app.onInit());
+//app.onInit();
+app.elobby = function () {
+
+	var hasWidget = true;
+	var atwoneDocType = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">';
+	var debug;
+	
+	/*------------------------------- ATDATA SCREEN FUNCTIONS -------------------------------*/
+	var utils = {
+	
+		isNumber: function (value) {
+			if ((undefined === value) || (null === value)) {
+				return false;
+			}
+			if (typeof value == 'number') {
+				return true;
+			}
+			return !isNaN(value - 0);
+		},
+
+		testfunc: function () {
+			alert('test func yoo!!');
+		}
+	};
+	
+	var _privateExec = {
+		utils.testfunc();
+	};
+	
+	function _privateConstruct(param) {
+		this.name = param;
+		alert('setting name as ' + param);
+	};
+	
+	return {
+		onInit : function(){
+			alert('yellow from init.!');
+			utils.isNumber('9') ? alert('yay it is number') : alert('nay it is not!');
+			utils.isNumber('waht about this') ? alert('take 2 - yay it is number') : alert('take 2 - nay it is not!');
+			alert(window);
+			alert(testPrivateConstruct.name);
+		},
+		
+		testPrivateConstruct : new _privateConstruct('myNameMiNem'),
+		
+		utils : utils
+	};
+}();
